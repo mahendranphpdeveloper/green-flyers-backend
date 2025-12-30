244a28a9cb3e69584de87afcd8384cd7b8449c7b
jayamwebbo3-pixel <jayamweb.bo3@gmail.com>
2025-12-30 10:15:04 +0530

    google login update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,61 +41,106 @@ class AuthController extends Controller
 
 
 
+    // public function googleLogin(Request $request)
+    // {
+    //     $request->validate([
+    //         'userEmail' => 'required|email',
+    //         'userName' => 'required|string',
+    //         'profilePic' => 'nullable|url',
+    //         'token' => 'required|string'
+    //     ]);
+
+    //     $email = $request->input('userEmail');
+    //     $name = $request->input('userName');
+    //     $profilePic = $request->input('profilePic');
+    //     $googleToken = $request->input('token');
+
+    //     $user = User::where('userEmail', $email)->first();
+
+    //     if (!$user) {
+    //         $user = User::create([
+    //             'userName' => $name,
+    //             'userEmail' => $email,
+    //             'profilePic' => $profilePic,
+    //             'google_token' => $googleToken,
+    //             // password not required for google login (auth happens by google token)
+    //         ]);
+    //     } else {
+    //         $updated = false;
+    //         if ($user->userName !== $name) {
+    //             $user->userName = $name;
+    //             $updated = true;
+    //         }
+    //         if ($profilePic && $user->profilePic !== $profilePic) {
+    //             $user->profilePic = $profilePic;
+    //             $updated = true;
+    //         }
+    //         if ($user->google_token !== $googleToken) {
+    //             $user->google_token = $googleToken;
+    //             $updated = true;
+    //         }
+    //         if ($updated) {
+    //             $user->save();
+    //         }
+    //     }
+
+    //     // Issue token using Laravel Sanctum
+    //     $token = $user->createToken('GreenFlyers_Token')->plainTextToken;
+
+    //     return response()->json([
+    //         'message' => 'Google login successful',
+    //         'token' => $token,
+    //         'user' => [
+    //             'userId' => $user->userId,
+    //             'name' => $user->userName,
+    //             'email' => $user->userEmail,
+    //             'profilePic' => $user->profilePic,
+    //         ]
+    //     ]);
+    // }
+
     public function googleLogin(Request $request)
-    {
-        $request->validate([
-            'userEmail' => 'required|email',
-            'userName' => 'required|string',
-            'profilePic' => 'nullable|url',
-            'token' => 'required|string'
+{
+    $request->validate([
+        'userEmail' => 'required|email',
+        'userName' => 'required|string',
+        'profilePic' => 'nullable|url',
+        'token' => 'required|string'
+    ]);
+
+    $email = $request->userEmail;
+    $name = $request->userName;
+    $profilePic = $request->profilePic;
+    $googleToken = $request->token;
+
+    $user = User::where('userEmail', $email)->first();
+
+    if (!$user) {
+        // FIRST TIME GOOGLE LOGIN
+        $user = User::create([
+            'userName'     => $name,
+            'userEmail'    => $email,
+            'profilePic'   => $profilePic,
+            'google_token' => $googleToken,
         ]);
-
-        $email = $request->input('userEmail');
-        $name = $request->input('userName');
-        $profilePic = $request->input('profilePic');
-        $googleToken = $request->input('token');
-
-        $user = User::where('userEmail', $email)->first();
-
-        if (!$user) {
-            $user = User::create([
-                'userName' => $name,
-                'userEmail' => $email,
-                'profilePic' => $profilePic,
-                'google_token' => $googleToken,
-                // password not required for google login (auth happens by google token)
-            ]);
-        } else {
-            $updated = false;
-            if ($user->userName !== $name) {
-                $user->userName = $name;
-                $updated = true;
-            }
-            if ($profilePic && $user->profilePic !== $profilePic) {
-                $user->profilePic = $profilePic;
-                $updated = true;
-            }
-            if ($user->google_token !== $googleToken) {
-                $user->google_token = $googleToken;
-                $updated = true;
-            }
-            if ($updated) {
-                $user->save();
-            }
-        }
-
-        // Issue token using Laravel Sanctum
-        $token = $user->createToken('GreenFlyers_Token')->plainTextToken;
-
-        return response()->json([
-            'message' => 'Google login successful',
-            'token' => $token,
-            'user' => [
-                'userId' => $user->userId,
-                'name' => $user->userName,
-                'email' => $user->userEmail,
-                'profilePic' => $user->profilePic,
-            ]
-        ]);
+    } else {
+        // EXISTING USER → DO NOT overwrite profile edits
+        $user->google_token = $googleToken;
+        $user->save();
     }
+
+    $token = $user->createToken('GreenFlyers_Token')->plainTextToken;
+
+    return response()->json([
+        'message' => 'Google login successful',
+        'token' => $token,
+        'user' => [
+            'userId'     => $user->userId,
+            'name'       => $user->userName,   // always DB value
+            'email'      => $user->userEmail,
+            'profilePic' => $user->profilePic, // always DB value
+        ]
+    ]);
+}
+
 }
