26970b74b3179a9772b55f88deba2e5dd31cfac4
jayamwebbo3-pixel <jayamweb.bo3@gmail.com>
2026-02-07 15:26:00 +0530

    session fixing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api([
             \Illuminate\Http\Middleware\HandleCors::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Session\Middleware\StartSession::class,
         ]);
 
     })
