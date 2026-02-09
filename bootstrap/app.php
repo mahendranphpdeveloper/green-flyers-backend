b1ced50ef46a0037268152cd36f670a81d34aacd
jayamwebbo3-pixel <jayamweb.bo3@gmail.com>
2026-02-09 18:23:03 +0530

    rollback

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,9 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->group('api', [
             \Illuminate\Http\Middleware\HandleCors::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \App\Http\Middleware\HandleAdminSession::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
 
